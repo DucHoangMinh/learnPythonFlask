1dba075855add9ddf2db33217db1e87a96e148ba
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-03-13 20:40:40 +0700

    Thêm secret key hiển thị form

--- main/__init__.py
+++ main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.app_context().push()
+app.config['SECRET_KEY'] = '4c14ff04a3c6d1ca1fff4b57'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
 db = SQLAlchemy(app)
