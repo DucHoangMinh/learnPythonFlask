1c3c65e2f5cfba11c3e6969e1c89ab9c95532f31
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-03-20 21:58:22 +0700

    finish advance validation and start user authentication

--- main/routes/routes.py
+++ main/routes/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from main.models.Item import Item
-from main import app
+from main import app, db
+from main.models.User import User
 from main.utils.forms import RegisterForm
 
 
@@ -14,7 +15,17 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('home_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'Error when creating user: {err_msg}', category="danger")
     return render_template('register.html', form=form)
--- main/utils/forms.py
+++ main/utils/forms.py
@@ -1,10 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
+from main.models.User import User
+
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username')
-    email_address = StringField(label='Email')
-    password1 = PasswordField(label='Password1')
-    password2 = PasswordField(label='Password2')
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already taken. Try other username!')
+    def validate_email_address(self, email_to_check):
+        user = User.query.filter_by(email_address=email_to_check.data).first()
+        if user:
+            raise ValidationError('Email already taken. Try other email!')
+
+    username = StringField(label='Username :', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email :', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password: ', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Submit')
 
